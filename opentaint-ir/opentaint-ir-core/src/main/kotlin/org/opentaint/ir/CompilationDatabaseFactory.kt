cf80978160337e48e198932aff1b07445f26ded2
Dmitry Mordvinov <dvvsrd@gmail.com>
2024-09-03 16:39:05 +0300

    add readme and fix the api

---
+++ opentaint-ir/opentaint-ir-core/src/main/kotlin/org/opentaint/ir/CompilationDatabaseFactory.kt
@@ -0,0 +1,11 @@
+package org.opentaint.ir
+
+import org.opentaint.ir.api.CompilationDatabase
+
+object CompilationDatabaseFactory {
+
+    suspend fun newDatabase(isPersisted: Boolean): CompilationDatabase {
+        TODO("implement me")
+    }
+
+}
---
+++ opentaint-ir/opentaint-ir-core/src/main/kotlin/org/opentaint/ir/api/Api.kt
@@ -0,0 +1,54 @@
+package org.opentaint.ir.api
+
+import java.io.File
+
+interface DatabaseClass {
+
+    val location: ByteCodeLocation
+
+    val name: String
+    val simpleName: String
+
+    val methods: List<DatabaseClassMethod>
+
+    val parents: List<DatabaseClass>
+    val interfaces: List<DatabaseClass>
+    val annotations: List<DatabaseClass>
+}
+
+interface ByteCodeLocation {
+
+    var isJar: Boolean
+
+    val file: File
+}
+
+interface DatabaseClassMethod {
+
+    val name: String
+    val signature: Any // todo return something meaning full
+
+    val annotations: List<DatabaseClass>
+    suspend fun readBody(): Any //TODO return something
+
+}
+
+interface ClasspathSet {
+
+    val locations: List<ByteCodeLocation>
+
+    suspend fun findClass(name: String): DatabaseClass?
+}
+
+
+interface CompilationDatabase {
+
+    suspend fun classpathSet(locations: List<File>): ClasspathSet
+
+    suspend fun load(dirOrJar: File)
+    suspend fun load(dirOrJars: List<File>)
+
+    suspend fun refresh()
+
+    fun watchFileSystemChanges()
+}
