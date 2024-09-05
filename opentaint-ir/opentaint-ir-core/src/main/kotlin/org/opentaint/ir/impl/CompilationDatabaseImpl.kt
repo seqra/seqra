ac8d8dd815aaabfd3bff98e6b538178f3d934255
Dmitry Mordvinov <dvvsrd@gmail.com>
2024-09-05 17:39:22 +0300

    #3 implement reading bytecode from compiled classes

--- opentaint-ir/opentaint-ir-core/src/main/kotlin/org/opentaint/ir/impl/CompilationDatabaseImpl.kt
+++ opentaint-ir/opentaint-ir-core/src/main/kotlin/org/opentaint/ir/impl/CompilationDatabaseImpl.kt
@@ -2,25 +2,44 @@ package org.opentaint.ir.impl
 
 import org.opentaint.ir.api.ClasspathSet
 import org.opentaint.ir.api.CompilationDatabase
+import org.opentaint.ir.impl.reader.ByteCodeReader
 import org.opentaint.ir.impl.tree.ClassTree
 import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.File
 
 class CompilationDatabaseImpl : CompilationDatabase {
 
     private val classTree = ClassTree()
+    private val byteCodeReader = ByteCodeReader()
 
     override suspend fun classpathSet(locations: List<File>): ClasspathSet {
         load(locations)
         return ClasspathSetImpl(locations.map { ByteCodeLocationImpl(it) }.toPersistentList(), classTree)
     }
 
-    override suspend fun load(dirOrJar: File) = with(this) {
+    override suspend fun load(dirOrJar: File) = apply {
         load(listOf(dirOrJar))
     }
 
-    override suspend fun load(dirOrJars: List<File>): CompilationDatabase {
-        TODO("Not yet implemented")
+    override suspend fun load(dirOrJars: List<File>) = apply {
+        dirOrJars.forEach {
+            if (!it.exists()) {
+                throw IllegalStateException("file or folder does not exists: ${it.absolutePath}")
+            }
+        }
+        withContext(Dispatchers.IO) {
+            dirOrJars.map { dirOrJar ->
+                launch(Dispatchers.IO) {
+                    byteCodeReader.readJar(dirOrJar).forEach {
+                        classTree.addClass(ByteCodeLocationImpl(dirOrJar), it.name, it)
+                    }
+                }
+            }
+        }.joinAll()
     }
 
     override suspend fun refresh(): CompilationDatabase {
