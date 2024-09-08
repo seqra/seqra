2ceaffbd5cc7a42ba8efd12bea069793734ec0e1
Dmitry Mordvinov <dvvsrd@gmail.com>
2024-09-08 14:03:17 +0300

    rename packages to org.opentaint

---
+++ opentaint-ir/opentaint-ir-core/src/main/kotlin/org/opentaint/java/compilation/database/impl/ClasspathSetImpl.kt
@@ -0,0 +1,32 @@
+package org.opentaint.java.compilation.database.impl
+
+import org.opentaint.java.compilation.database.api.ByteCodeLocation
+import org.opentaint.java.compilation.database.api.ClassId
+import org.opentaint.java.compilation.database.api.ClasspathSet
+import org.opentaint.java.compilation.database.impl.tree.ClassTree
+import org.opentaint.java.compilation.database.impl.tree.ClasspathClassTree
+
+class ClasspathSetImpl(
+    override val locations: List<ByteCodeLocation>,
+    private val db: CompilationDatabaseImpl,
+    classTree: ClassTree
+) : ClasspathSet {
+
+    private val classpathClassTree = ClasspathClassTree(classTree, locations)
+    private val classIdService = ClassIdService(classpathClassTree)
+
+    override suspend fun findClassOrNull(name: String): ClassId? {
+        return classIdService.toClassId(classpathClassTree.firstClassOrNull(name))
+    }
+
+    override suspend fun findSubTypesOf(name: String): List<ClassId> {
+        db.awaitBackgroundJobs()
+        return classpathClassTree.findSubTypesOf(name)
+            .map { classIdService.toClassId(it) }
+            .filterNotNull()
+    }
+
+    override fun close() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ opentaint-ir/opentaint-ir-core/src/main/kotlin/org/opentaint/java/compilation/database/impl/tree/ClasspathClassTree.kt
@@ -0,0 +1,22 @@
+package org.opentaint.java.compilation.database.impl.tree
+
+import org.opentaint.java.compilation.database.api.ByteCodeLocation
+
+class ClasspathClassTree(
+    private val classTree: ClassTree,
+    locations: List<ByteCodeLocation>
+) {
+
+    private val locationHashes = locations.map { it.version }.toHashSet()
+    fun firstClassOrNull(fullName: String): ClassNode? {
+        return classTree.firstClassNodeOrNull(fullName) {
+            locationHashes.contains(it)
+        }
+    }
+
+    fun findSubTypesOf(fullName: String): List<ClassNode> {
+        return firstClassOrNull(fullName)?.subTypes.orEmpty().filter {
+            locationHashes.contains(it.location.version)
+        }
+    }
+}
