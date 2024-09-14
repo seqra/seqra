76ccc370e0b3c0939d8241d27691d4312e4c0996
Dmitry Mordvinov <dvvsrd@gmail.com>
2024-09-14 18:13:46 +0300

    Add indexes api #16

---
+++ opentaint-ir/opentaint-ir-core/src/main/kotlin/org/opentaint/java/compilation/database/impl/index/Index.kt
@@ -0,0 +1,26 @@
+package org.opentaint.java.compilation.database.impl.index
+
+import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.MethodNode
+import org.opentaint.java.compilation.database.api.ByteCodeLocation
+
+interface ByteCodeLocationIndexBuilder<T> {
+
+    val location: ByteCodeLocation
+
+    fun index(classNode: ClassNode)
+
+    fun index(classNode: ClassNode, methodNode: MethodNode)
+
+    fun build(): ByteCodeLocationIndex<T>
+
+}
+
+interface ByteCodeLocationIndex<T> {
+
+    val key: String
+    val location: ByteCodeLocation
+
+    fun query(term: String): Sequence<T>
+}
+
