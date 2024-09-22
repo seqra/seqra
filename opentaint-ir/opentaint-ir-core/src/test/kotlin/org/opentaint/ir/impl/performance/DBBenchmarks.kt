37bca1dd441c4b0df707c6b126332285e7a8661d
Dmitry Mordvinov <dvvsrd@gmail.com>
2024-09-22 11:52:16 +0300

    migrate to jmh

---
+++ opentaint-ir/opentaint-ir-core/src/test/kotlin/org/opentaint/ir/impl/performance/DBBenchmarks.kt
@@ -0,0 +1,65 @@
+package org.opentaint.ir.impl.performance
+
+
+import kotlinx.coroutines.runBlocking
+import org.openjdk.jmh.annotations.*
+import org.opentaint.ir.api.CompilationDatabase
+import org.opentaint.ir.compilationDatabase
+import org.opentaint.ir.impl.LibrariesMixin
+import org.opentaint.ir.impl.fs.asByteCodeLocation
+import org.opentaint.ir.impl.fs.load
+import org.opentaint.ir.impl.index.ReversedUsagesIndex
+import org.opentaint.ir.impl.tree.ClassTree
+import java.util.concurrent.TimeUnit
+
+
+@State(Scope.Benchmark)
+@Fork(0)
+@Warmup(iterations = 2)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
+class DBBenchmarks : LibrariesMixin {
+
+    private var db: CompilationDatabase? = null
+
+    @Benchmark
+    fun readBytecode() {
+        val lib = guavaLib
+        runBlocking {
+            lib.asByteCodeLocation().loader()!!.load(ClassTree())
+        }
+    }
+
+    @Benchmark
+    fun readingJVMbytecode() {
+        db = runBlocking {
+            compilationDatabase {
+                useProcessJavaRuntime()
+
+                installIndexes(ReversedUsagesIndex)
+            }
+        }
+    }
+
+    @Benchmark
+    fun readingJVMbytecodeWithProjectClasspath() {
+        db = runBlocking {
+            compilationDatabase {
+                useProcessJavaRuntime()
+                predefinedDirOrJars = allJars
+                installIndexes(ReversedUsagesIndex)
+            }
+        }
+    }
+
+    @TearDown
+    fun tearDown() {
+        db?.let {
+            runBlocking {
+                it.awaitBackgroundJobs()
+                it.close()
+            }
+        }
+    }
+}
---
+++ opentaint-ir/opentaint-ir-core/src/test/kotlin/org/opentaint/ir/impl/performance/DBLifeCycleBenchmarks.kt
@@ -0,0 +1,53 @@
+package org.opentaint.ir.impl.performance
+
+import kotlinx.coroutines.runBlocking
+import org.openjdk.jmh.annotations.*
+import org.opentaint.ir.api.CompilationDatabase
+import org.opentaint.ir.compilationDatabase
+import org.opentaint.ir.impl.LibrariesMixin
+import org.opentaint.ir.impl.index.ReversedUsagesIndex
+import java.util.concurrent.TimeUnit
+
+@State(Scope.Benchmark)
+@Fork(0)
+@Warmup(iterations = 2)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
+class DBLifeCycleBenchmarks : LibrariesMixin {
+
+    private lateinit var db: CompilationDatabase
+
+    @Setup(Level.Iteration)
+    fun setup() {
+        db = runBlocking {
+            compilationDatabase {
+                installIndexes(ReversedUsagesIndex)
+                useProcessJavaRuntime()
+            }
+        }
+    }
+
+    @Benchmark
+    fun loadAdditionalJars() {
+        val jars = allJars
+        runBlocking {
+            db.load(jars)
+        }
+    }
+
+    @Benchmark
+    fun awaitIndexing() {
+        runBlocking {
+            db.awaitBackgroundJobs()
+        }
+    }
+
+    @TearDown
+    fun tearDown() {
+        runBlocking {
+            db.awaitBackgroundJobs()
+            db.close()
+        }
+    }
+}
