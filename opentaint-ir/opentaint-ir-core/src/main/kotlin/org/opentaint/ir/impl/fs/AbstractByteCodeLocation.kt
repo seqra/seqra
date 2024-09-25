f303204f31ae87a1de876d57a2cb34407d06c3db
Dmitry Mordvinov <dvvsrd@gmail.com>
2024-09-25 13:10:25 +0300

    expose access to `file` of ByteCodeLocation

--- opentaint-ir/opentaint-ir-core/src/main/kotlin/org/opentaint/ir/impl/fs/AbstractByteCodeLocation.kt
+++ opentaint-ir/opentaint-ir-core/src/main/kotlin/org/opentaint/ir/impl/fs/AbstractByteCodeLocation.kt
@@ -1,12 +1,17 @@
 package org.opentaint.ir.impl.fs
 
 import org.opentaint.ir.api.ByteCodeLocation
+import java.io.File
+import java.net.URL
 
-abstract class AbstractByteCodeLocation : ByteCodeLocation {
+abstract class AbstractByteCodeLocation(protected val file: File) : ByteCodeLocation {
 
     override val id: String by lazy(LazyThreadSafetyMode.NONE) {
         getCurrentId()
     }
+
+    override val locationURL: URL
+        get() = file.toURI().toURL()
 
     abstract fun getCurrentId(): String
 
--- opentaint-ir/opentaint-ir-core/src/test/kotlin/org/opentaint/ir/impl/tree/DummyCodeLocation.kt
+++ opentaint-ir/opentaint-ir-core/src/test/kotlin/org/opentaint/ir/impl/tree/DummyCodeLocation.kt
@@ -3,11 +3,15 @@ package org.opentaint.ir.impl.tree
 import org.opentaint.ir.api.ByteCodeLocation
 import org.opentaint.ir.api.LocationScope
 import org.opentaint.ir.impl.fs.ByteCodeLoaderImpl
+import java.net.URL
 
 open class DummyCodeLocation(override val id: String) : ByteCodeLocation {
 
     override val scope: LocationScope
         get() = LocationScope.APP
+
+    override val locationURL: URL
+        get() = TODO()
 
     override fun isChanged() = false
 
