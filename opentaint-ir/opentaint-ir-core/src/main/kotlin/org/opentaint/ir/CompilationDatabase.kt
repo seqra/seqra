93292e1f1ad0bd81c73c58541728646f08aa2a15
Dmitry Mordvinov <dvvsrd@gmail.com>
2024-09-27 13:17:49 +0300

    tests for persistence #11

--- opentaint-ir/opentaint-ir-core/src/main/kotlin/org/opentaint/ir/CompilationDatabase.kt
+++ opentaint-ir/opentaint-ir-core/src/main/kotlin/org/opentaint/ir/CompilationDatabase.kt
@@ -2,7 +2,6 @@ package org.opentaint.ir
 
 import org.opentaint.ir.api.CompilationDatabase
 import org.opentaint.ir.impl.CompilationDatabaseImpl
-import org.opentaint.ir.impl.FeaturesRegistry
 import org.opentaint.ir.impl.fs.JavaRuntime
 import org.opentaint.ir.impl.fs.asByteCodeLocation
 import org.opentaint.ir.impl.storage.PersistentEnvironment
@@ -11,26 +10,27 @@ import java.io.File
 suspend fun compilationDatabase(builder: CompilationDatabaseSettings.() -> Unit): CompilationDatabase {
     val settings = CompilationDatabaseSettings().also(builder)
     val persistentSettings = settings.persistentSettings
-    val features = settings.fullFeatures
     if (persistentSettings != null) {
         val environment = persistentSettings.toEnvironment()
         if (environment != null) {
             val restoredLocations = environment.allByteCodeLocations
-            val restoredFeatures = environment.restore(features, restoredLocations.toList())
-            val notLoaded = (JavaRuntime(settings.jre).allLocations + settings.predefinedDirOrJars.map {
-                it.asByteCodeLocation(isRuntime = false)
-            }).toSet() - restoredLocations.toSet()
+            val byteCodeLocations = restoredLocations.map { it.second }.toList()
+            val notLoaded = (
+                    JavaRuntime(settings.jre).allLocations +
+                            settings.predefinedDirOrJars
+                                .filter { it.exists() }
+                                .map { it.asByteCodeLocation(isRuntime = false) }
+                    ).toSet() - byteCodeLocations.toSet()
             val database = CompilationDatabaseImpl(
                 persistentEnvironment = environment,
-                settings = settings,
-                featureRegistry = restoredFeatures
+                settings = settings
             )
-
+            database.restoreDataFrom(restoredLocations.toMap())
             database.loadLocations(notLoaded.toList())
             return database
         }
     }
-    val database = CompilationDatabaseImpl(null, settings, FeaturesRegistry(null, settings.fullFeatures))
+    val database = CompilationDatabaseImpl(null, settings)
     database.loadJavaLibraries()
     if (settings.predefinedDirOrJars.isNotEmpty()) {
         database.load(settings.predefinedDirOrJars)
