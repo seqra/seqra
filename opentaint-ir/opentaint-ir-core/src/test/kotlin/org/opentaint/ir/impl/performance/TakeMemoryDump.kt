2646cab4b88c8bed5470a92ada9752c98163e804
Dmitry Mordvinov <dvvsrd@gmail.com>
2024-11-13 15:36:53 +0300

    cleanup

--- opentaint-ir/opentaint-ir-core/src/test/kotlin/org/opentaint/ir/impl/performance/TakeMemoryDump.kt
+++ opentaint-ir/opentaint-ir-core/src/test/kotlin/org/opentaint/ir/impl/performance/TakeMemoryDump.kt
@@ -1,27 +1,31 @@
 package org.opentaint.ir
 
 import kotlinx.coroutines.runBlocking
-import org.opentaint.ir.impl.index.Usages
+import org.opentaint.ir.impl.allClasspath
+import org.opentaint.ir.impl.storage.jooq.tables.references.CLASSES
+import org.opentaint.ir.impl.storage.jooq.tables.references.FIELDS
+import org.opentaint.ir.impl.storage.jooq.tables.references.METHODPARAMETERS
+import org.opentaint.ir.impl.storage.jooq.tables.references.METHODS
 
 fun main() {
     var start = System.currentTimeMillis()
     runBlocking {
         val db = jirdb {
-//            predefinedDirOrJars = allClasspath
+            loadByteCode(allClasspath)
             persistent("D:\\work\\jirdb\\jirdb.db")
-            installFeatures(Usages)
+//            installFeatures(Usages)
         }.also {
             println("AWAITING db took ${System.currentTimeMillis() - start}ms")
             start = System.currentTimeMillis()
             it.awaitBackgroundJobs()
             println("AWAITING jobs took ${System.currentTimeMillis() - start}ms")
         }
-//        transaction {
-//            println("Classes " + ClassEntity.count())
-//            println("Methods " + MethodEntity.count())
-//            println("Methods params " + MethodParameterEntity.count())
-//            println("Fields " + FieldEntity.count())
-//        }
+        db.persistence.read {
+            println("Classes " + it.fetchCount(CLASSES))
+            println("Methods " + it.fetchCount(METHODS))
+            println("Methods params "+ it.fetchCount(METHODPARAMETERS))
+            println("Fields " + it.fetchCount(FIELDS))
+        }
 
 //        val name = ManagementFactory.getRuntimeMXBean().name
 //        val pid = name.split("@")[0]
@@ -31,12 +35,3 @@ fun main() {
         println(db)
     }
 }
-//
-//fun main() {
-//    println(db)
-//    val name = ManagementFactory.getRuntimeMXBean().name
-//    val pid = name.split("@")[0]
-//    println("Taking memory dump from $pid....")
-//    val process = Runtime.getRuntime().exec("jmap -dump:live,format=b,file=db.hprof $pid")
-//    process.waitFor()
-//}
