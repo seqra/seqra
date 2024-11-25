1dea87268f7d2013e5e87955fadb56e025ae4768
Dmitry Mordvinov <dvvsrd@gmail.com>
2024-11-25 17:26:46 +0300

    switch to gradle plugin

--- opentaint-ir/settings.gradle.kts
+++ opentaint-ir/settings.gradle.kts
@@ -1,5 +1,17 @@
 rootProject.name = "jirdb"
 
+plugins {
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.0.25"
+}
+
+gitHooks {
+    preCommit {
+        // Content can be added at the bottom of the script
+        from(file("pre-commit").toURI().toURL())
+    }
+    createHooks() // actual hooks creation
+}
+
 include("opentaint-ir-api")
 include("opentaint-ir-core")
 include("jirdb-testing")
