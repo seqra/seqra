3aa7dd8af293816dd0bb19e4761fa6c566d02af3
Dmitry Mordvinov <dvvsrd@gmail.com>
2024-12-16 12:53:54 +0300

    fix tests

--- opentaint-ir/opentaint-ir-core/src/testFixtures/java/org/opentaint/ir/testing/cfg/JavaTasks.java
+++ opentaint-ir/opentaint-ir-core/src/testFixtures/java/org/opentaint/ir/testing/cfg/JavaTasks.java
@@ -2,7 +2,11 @@ package org.opentaint.ir.testing.cfg;
 
 import kotlin.NotImplementedError;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
