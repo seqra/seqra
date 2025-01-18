28ee3de2d3a93248530840887390231826ffb1dc
Dmitry Mordvinov <dvvsrd@gmail.com>
2025-01-18 14:43:33 +0300

    Low algorithm for detecting changes in BuildFolder closes #70

--- opentaint-ir/opentaint-ir-core/src/main/kotlin/org/opentaint/ir/impl/fs/AbstractByteCodeLocation.kt
+++ opentaint-ir/opentaint-ir-core/src/main/kotlin/org/opentaint/ir/impl/fs/AbstractByteCodeLocation.kt
@@ -1,7 +1,9 @@
 package org.opentaint.ir.impl.fs
 
+import com.google.common.hash.Hashing
 import org.opentaint.ir.api.JIRByteCodeLocation
 import java.io.File
+import java.nio.charset.StandardCharsets
 
 abstract class AbstractByteCodeLocation(override val jarOrFolder: File) : JIRByteCodeLocation {
 
@@ -11,5 +13,12 @@ abstract class AbstractByteCodeLocation(override val jarOrFolder: File) : JIRByt
     abstract fun currentHash(): String
 
     override fun isChanged() = fileSystemId != currentHash()
+
+    protected val String.shaHash: String
+        get() {
+            return Hashing.sha256()
+                .hashString(this, StandardCharsets.UTF_8)
+                .toString();
+        }
 
 }
