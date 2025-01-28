265a78eb798f9b78cabbe5b627e1cbfc9db08793
Dmitry Mordvinov <dvvsrd@gmail.com>
2025-01-28 13:21:43 +0300

    fix missed cfg/instructions caching

--- opentaint-ir/opentaint-ir-core/src/main/kotlin/org/opentaint/ir/impl/features/classpaths/MethodInstructionsFeature.kt
+++ opentaint-ir/opentaint-ir-core/src/main/kotlin/org/opentaint/ir/impl/features/classpaths/MethodInstructionsFeature.kt
@@ -1,5 +1,6 @@
 package org.opentaint.ir.impl.features.classpaths
 
+import org.opentaint.ir.api.JIRFeatureEvent
 import org.opentaint.ir.api.JIRInstExtFeature
 import org.opentaint.ir.api.JIRMethod
 import org.opentaint.ir.api.JIRMethodExtFeature
@@ -10,6 +11,7 @@ import org.opentaint.ir.api.cfg.JIRRawInst
 import org.opentaint.ir.impl.cfg.JIRGraphImpl
 import org.opentaint.ir.impl.cfg.JIRInstListBuilder
 import org.opentaint.ir.impl.cfg.RawInstListBuilder
+import org.opentaint.ir.impl.features.JIRFeatureEventImpl
 
 object MethodInstructionsFeature : JIRMethodExtFeature {
 
@@ -32,6 +34,10 @@ object MethodInstructionsFeature : JIRMethodExtFeature {
         return method.methodFeatures.fold(list) { value, feature ->
             feature.transformRawInstList(method, value)
         }
+    }
+
+    override fun event(result: Any, input: Array<Any>): JIRFeatureEvent {
+        return JIRFeatureEventImpl(this, result, input)
     }
 
 }
