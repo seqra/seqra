8a4e7e300a81b995deb514ce6c06431551e67cc7
Dmitry Mordvinov <dvvsrd@gmail.com>
2025-02-04 13:21:15 +0300

    generate global documentation only for published modules

--- opentaint-ir/build.gradle.kts
+++ opentaint-ir/build.gradle.kts
@@ -108,7 +108,6 @@ allprojects {
             reports {
                 csv.required.set(true)
             }
-
         }
 
         withType<Test> {
@@ -118,24 +117,6 @@ allprojects {
                 events("passed", "skipped", "failed")
             }
             finalizedBy(jacocoTestReport) // report is always generated after tests run
-        }
-
-        val dokkaJavadocJar by creating(Jar::class) {
-            dependsOn(dokkaJavadoc)
-            from(dokkaJavadoc.flatMap { it.outputDirectory })
-            archiveClassifier.set("javadoc")
-        }
-
-        val sourcesJar by creating(Jar::class) {
-            archiveClassifier.set("sources")
-            from(sourceSets.getByName("main").kotlin.srcDirs)
-        }
-
-        artifacts {
-            archives(sourcesJar)
-            if (includeDokka != null) {
-                archives(dokkaJavadocJar)
-            }
         }
     }
 
@@ -148,22 +129,6 @@ allprojects {
         include("**/*.java")
         header(rootProject.file("docs/copyright/COPYRIGHT_HEADER.txt"))
     }
-
-    publishing {
-        publications {
-            register<MavenPublication>("jar") {
-                from(components["java"])
-                artifact(tasks.named("sourcesJar"))
-                artifact(tasks.named("dokkaJavadocJar"))
-
-                groupId = "org.opentaint.ir"
-                artifactId = project.name
-                addPom()
-                signPublication(this@allprojects)
-            }
-        }
-    }
-
 }
 
 val repoUrl: String? = project.properties["repoUrl"] as? String ?: "https://maven.pkg.github.com/Opentaint/opentaint-ir"
@@ -177,7 +142,40 @@ if (!repoUrl.isNullOrEmpty()) {
             project(":opentaint-ir-approximations"),
         )
     ) {
+        tasks {
+            val dokkaJavadocJar by creating(Jar::class) {
+                dependsOn(dokkaJavadoc)
+                from(dokkaJavadoc.flatMap { it.outputDirectory })
+                archiveClassifier.set("javadoc")
+            }
+
+            val sourcesJar by creating(Jar::class) {
+                archiveClassifier.set("sources")
+                from(sourceSets.getByName("main").kotlin.srcDirs)
+            }
+
+            artifacts {
+                archives(sourcesJar)
+                if (includeDokka != null) {
+                    archives(dokkaJavadocJar)
+                }
+            }
+
+        }
         publishing {
+            publications {
+                register<MavenPublication>("jar") {
+                    from(components["java"])
+                    artifact(tasks.named("sourcesJar"))
+                    artifact(tasks.named("dokkaJavadocJar"))
+
+                    groupId = "org.opentaint.ir"
+                    artifactId = project.name
+                    addPom()
+                    signPublication(this@configure)
+                }
+            }
+
             repositories {
                 maven {
                     name = "repo"
@@ -191,6 +189,20 @@ if (!repoUrl.isNullOrEmpty()) {
                     }
                 }
             }
+        }
+    }
+}
+
+configure(listOf(rootProject)) {
+    tasks {
+        dokkaHtmlMultiModule {
+            removeChildTasks(
+                listOf(
+                    project(":opentaint-ir-examples"),
+                    project(":opentaint-ir-cli"),
+                    project(":opentaint-ir-benchmarks")
+                )
+            )
         }
     }
 }
