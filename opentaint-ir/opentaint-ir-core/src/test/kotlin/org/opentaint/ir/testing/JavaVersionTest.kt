ae28646b0b6f5517e68825261bd064a041cc765a
Dmitry Mordvinov <dvvsrd@gmail.com>
2025-02-04 14:20:10 +0300

    Parse java version properly #99 (#102)

---
+++ opentaint-ir/opentaint-ir-core/src/test/kotlin/org/opentaint/ir/testing/JavaVersionTest.kt
@@ -0,0 +1,23 @@
+package org.opentaint.ir.testing
+
+import org.opentaint.ir.impl.JIRSettings
+import org.opentaint.ir.impl.fs.JavaRuntime
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.EnabledOnJre
+import org.junit.jupiter.api.condition.JRE
+
+class JavaVersionTest {
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_11)
+    fun `java version should be proper for 11 java`() {
+        assertEquals(11, JavaRuntime(JIRSettings().useProcessJavaRuntime().jre).version.majorVersion)
+    }
+    @Test
+    @EnabledOnJre(JRE.JAVA_8)
+    fun `java version should be proper for 8 java`() {
+        assertEquals(8, JavaRuntime(JIRSettings().useProcessJavaRuntime().jre).version.majorVersion)
+    }
+
+}
