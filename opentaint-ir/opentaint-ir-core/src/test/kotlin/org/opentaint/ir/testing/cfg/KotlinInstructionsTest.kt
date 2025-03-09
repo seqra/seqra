98a0931715c6c8b3b614f8f342ac6d6fadd1d579
Daniil Logunov <43185213+danielelog@users.noreply.github.com>
2025-03-09 13:01:17 +0300

    Kotlin tests added

---
+++ opentaint-ir/opentaint-ir-core/src/test/kotlin/org/opentaint/ir/testing/cfg/KotlinInstructionsTest.kt
@@ -0,0 +1,27 @@
+package org.opentaint.ir.testing.cfg
+
+import org.opentaint.ir.testing.WithDB
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class KotlinInstructionsTest: BaseInstructionsTest() {
+
+    companion object : WithDB()
+
+    private fun runTest(className: String) {
+        val clazz = cp.findClassOrNull(className)
+        Assertions.assertNotNull(clazz)
+
+        val javaClazz = testAndLoadClass(clazz!!)
+        val clazzInstance = javaClazz.constructors.first().newInstance()
+        val method = javaClazz.methods.first { it.name == "box" }
+        val res = method.invoke(clazzInstance)
+        Assertions.assertEquals("OK", res)
+    }
+
+    @Test
+    fun `simple test`() = runTest(SimpleTest::class.java.name)
+
+    @Test
+    fun `kotlin vararg test`() = runTest(Varargs::class.java.name)
+}
---
+++ opentaint-ir/opentaint-ir-core/src/testFixtures/kotlin/org/opentaint/ir/testing/cfg/SimpleTest.kt
@@ -0,0 +1,8 @@
+package org.opentaint.ir.testing.cfg
+
+class SimpleTest {
+
+    fun box(): String {
+        return "OK"
+    }
+}
