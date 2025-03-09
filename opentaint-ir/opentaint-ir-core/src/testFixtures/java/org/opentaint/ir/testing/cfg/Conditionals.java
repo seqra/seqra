8c25dc7475256892f2ef7e7e601a438c95a29bbd
Daniil Logunov <43185213+danielelog@users.noreply.github.com>
2025-03-09 17:36:58 +0300

    Added test with lambda

--- opentaint-ir/opentaint-ir-core/src/testFixtures/java/org/opentaint/ir/testing/cfg/Conditionals.java
+++ opentaint-ir/opentaint-ir-core/src/testFixtures/java/org/opentaint/ir/testing/cfg/Conditionals.java
@@ -21,7 +21,7 @@ public class Conditionals {
         }
     }
 
-    public static void conditionInFor() {
+    public static Object conditionInFor() {
         Random rnd = new Random();
         List<Boolean> list = new ArrayList<>();
         int numFalse = 0;
@@ -40,6 +40,7 @@ public class Conditionals {
         for (int i = numFalse; i < 1000; i++)
             if (!list.get(i))
                 throw new RuntimeException("False negative: " + i);
+        return null;
     }
 
 }
