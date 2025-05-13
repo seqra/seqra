ff34e90d3d0b7b104865512a4d18b20d1e4b6345
Pavel Balay <71063977+Bupaheh@users.noreply.github.com>
2025-05-13 12:33:44 +0300

    Reuse event instances

--- opentaint-ir/opentaint-ir-analysis/src/main/kotlin/org/opentaint/ir/analysis/ifds2/RunnerImpl.kt
+++ opentaint-ir/opentaint-ir-analysis/src/main/kotlin/org/opentaint/ir/analysis/ifds2/RunnerImpl.kt
@@ -51,6 +51,9 @@ class RunnerImpl<Fact, Event>(
     private val summaryEdges = hashMapOf<Vertex<Fact>, HashSet<Vertex<Fact>>>()
     private val callerPathEdgeOf = hashMapOf<Vertex<Fact>, HashSet<Edge<Fact>>>()
 
+    private val queueIsEmpty = QueueEmptinessChanged(runner = this, isEmpty = true)
+    private val queueIsNotEmpty = QueueEmptinessChanged(runner = this, isEmpty = false)
+
     private val Edge<Fact>.reasons: List<Reason>
         get() = this@RunnerImpl.reasons[this]!!.toList()
 
@@ -110,7 +113,7 @@ class RunnerImpl<Fact, Event>(
 
             // Add edge to worklist:
             if (workList.isEmpty) {
-                manager.handleControlEvent(QueueEmptinessChanged(this@RunnerImpl, false))
+                manager.handleControlEvent(queueIsNotEmpty)
             }
             workList.trySend(edge).getOrThrow()
 
@@ -123,7 +126,7 @@ class RunnerImpl<Fact, Event>(
     private suspend fun tabulationAlgorithm() = coroutineScope {
         while (isActive) {
             val edge = workList.tryReceive().getOrElse {
-                manager.handleControlEvent(QueueEmptinessChanged(this@RunnerImpl, true))
+                manager.handleControlEvent(queueIsEmpty)
                 val edge = workList.receive()
                 edge
             }
