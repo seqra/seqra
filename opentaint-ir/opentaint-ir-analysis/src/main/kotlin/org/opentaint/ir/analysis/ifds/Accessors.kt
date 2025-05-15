2884de7d8658538843167a1a80c97bd7a91e16b0
Pavel Balay <71063977+Bupaheh@users.noreply.github.com>
2025-05-15 23:42:15 +0300

    Compact toString

--- opentaint-ir/opentaint-ir-analysis/src/main/kotlin/org/opentaint/ir/analysis/ifds/Accessors.kt
+++ opentaint-ir/opentaint-ir-analysis/src/main/kotlin/org/opentaint/ir/analysis/ifds/Accessors.kt
@@ -1,7 +1,6 @@
 package org.opentaint.ir.analysis.ifds
 
 import org.opentaint.ir.api.JIRField
-import org.opentaint.ir.api.cfg.JIRValue
 
 sealed interface Accessor {
     fun toSuffix(): String
@@ -10,21 +9,11 @@ sealed interface Accessor {
 data class FieldAccessor(
     val field: JIRField,
 ) : Accessor {
-    override fun toSuffix(): String {
-        return ".${field.name}"
-    }
-
-    override fun toString(): String {
-        return field.name
-    }
+    override fun toSuffix(): String = ".${field.name}"
+    override fun toString(): String = field.name
 }
 
 object ElementAccessor : Accessor {
-    override fun toSuffix(): String {
-        return "[*]"
-    }
-
-    override fun toString(): String {
-        return "*"
-    }
+    override fun toSuffix(): String = "[*]"
+    override fun toString(): String = "*"
 }
