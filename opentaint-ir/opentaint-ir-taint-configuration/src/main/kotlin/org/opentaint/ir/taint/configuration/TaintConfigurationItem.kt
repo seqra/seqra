32d09210eeaa6ca04b9ff9f76e99902c7386f4de
Pavel Balay <71063977+Bupaheh@users.noreply.github.com>
2025-06-18 11:54:11 +0300

    Replace JIRMethod with CommonMethod, eliminate mock

--- opentaint-ir/opentaint-ir-taint-configuration/src/main/kotlin/org/opentaint/ir/taint/configuration/TaintConfigurationItem.kt
+++ opentaint-ir/opentaint-ir-taint-configuration/src/main/kotlin/org/opentaint/ir/taint/configuration/TaintConfigurationItem.kt
@@ -1,36 +1,36 @@
 package org.opentaint.ir.taint.configuration
 
-import org.opentaint.ir.api.jvm.JIRMethod
+import org.opentaint.ir.api.common.CommonMethod
 
 sealed interface TaintConfigurationItem
 
 data class TaintEntryPointSource(
-    val method: JIRMethod,
+    val method: CommonMethod<*, *>,
     val condition: Condition,
     val actionsAfter: List<Action>,
 ) : TaintConfigurationItem
 
 data class TaintMethodSource(
-    val method: JIRMethod,
+    val method: CommonMethod<*, *>,
     val condition: Condition,
     val actionsAfter: List<Action>,
 ) : TaintConfigurationItem
 
 data class TaintMethodSink(
-    val method: JIRMethod,
+    val method: CommonMethod<*, *>,
     val ruleNote: String,
     val cwe: List<Int>,
     val condition: Condition,
 ) : TaintConfigurationItem
 
 data class TaintPassThrough(
-    val method: JIRMethod,
+    val method: CommonMethod<*, *>,
     val condition: Condition,
     val actionsAfter: List<Action>,
 ) : TaintConfigurationItem
 
 data class TaintCleaner(
-    val method: JIRMethod,
+    val method: CommonMethod<*, *>,
     val condition: Condition,
     val actionsAfter: List<Action>,
 ) : TaintConfigurationItem
