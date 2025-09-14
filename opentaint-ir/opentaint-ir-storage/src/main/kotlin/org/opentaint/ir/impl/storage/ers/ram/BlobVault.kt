846b85b0c0c4632466c6d45c533698b81f4dd649
Dmitry Mordvinov <dvvsrd@gmail.com>
2025-09-14 17:52:26 +0300

    [opentaint-ir-storage] Add BlobVault for blobs' deduplication in RAMDataContainerMutable

---
+++ opentaint-ir/opentaint-ir-storage/src/main/kotlin/org/opentaint/ir/impl/storage/ers/ram/BlobVault.kt
@@ -0,0 +1,28 @@
+package org.opentaint.ir.impl.storage.ers.ram
+
+import jetbrains.exodus.core.dataStructures.hash.IntHashMap
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
+
+internal object BlobVault {
+
+    private val blobs = IntHashMap<MutableList<ByteArray>>()
+    private val lock = ReentrantLock()
+
+    internal fun ByteArray.asUnique(): ByteArray {
+        val hc = this.contentHashCode()
+        lock.withLock {
+            val arrays = blobs[hc] ?: arrayListOf()
+            arrays.forEach { array ->
+                if (this contentEquals array) {
+                    return array
+                }
+            }
+            arrays.add(this)
+            if (arrays.size == 1) {
+                blobs[hc] = arrays
+            }
+            return this
+        }
+    }
+}
--- opentaint-ir/opentaint-ir-storage/src/main/kotlin/org/opentaint/ir/impl/storage/ers/ram/TinyBlobsCache.kt
+++ opentaint-ir/opentaint-ir-storage/src/main/kotlin/org/opentaint/ir/impl/storage/ers/ram/TinyBlobsCache.kt
@@ -1,10 +1,12 @@
 package org.opentaint.ir.impl.storage.ers.ram
 
+import org.opentaint.ir.impl.storage.ers.ram.BlobVault.asUnique
+
 internal fun ByteArray.probablyCached(): ByteArray {
     return if (size == 1) {
         theCache[this[0].toInt() and 0xff]
     } else {
-        this
+        this.asUnique()
     }
 }
 
