6c257a942795f0fb58387fcac08b2596ff39d535
Daniil Logunov <43185213+danielelog@users.noreply.github.com>
2025-12-30 17:09:54 +0300

    Expand SourceFileResolver interface (#15)
    * Expand SourceFileResolver interface
    
    * Update opentaint-ir version

--- opentaint-dataflow-core/opentaint-dataflow/src/main/kotlin/org/opentaint/dataflow/sarif/SourceFileResolver.kt
+++ opentaint-dataflow-core/opentaint-dataflow/src/main/kotlin/org/opentaint/dataflow/sarif/SourceFileResolver.kt
@@ -2,6 +2,8 @@ package org.opentaint.dataflow.sarif
 
 import org.opentaint.ir.api.common.cfg.CommonInst
 
-fun interface SourceFileResolver<in Statement : CommonInst> {
-    fun resolve(inst: Statement): String?
+interface SourceFileResolver<in Statement : CommonInst> {
+    fun resolveByName(inst: Statement, pkg: String, name: String): String?
+
+    fun resolveByInst(inst: Statement): String?
 }
