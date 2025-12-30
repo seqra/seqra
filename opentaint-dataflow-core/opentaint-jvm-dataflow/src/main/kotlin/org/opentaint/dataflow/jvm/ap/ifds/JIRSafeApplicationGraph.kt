4962ae1f4a63ceeb2638fcfb593d0d2b284f92f0
Valentyn Sobol <8640896+saloed@users.noreply.github.com>
2025-12-30 17:56:22 +0300

    Fix logging

--- opentaint-dataflow-core/opentaint-jvm-dataflow/src/main/kotlin/org/opentaint/dataflow/jvm/ap/ifds/JIRSafeApplicationGraph.kt
+++ opentaint-dataflow-core/opentaint-jvm-dataflow/src/main/kotlin/org/opentaint/dataflow/jvm/ap/ifds/JIRSafeApplicationGraph.kt
@@ -1,5 +1,6 @@
 package org.opentaint.dataflow.jvm.ap.ifds
 
+import mu.KLogging
 import org.opentaint.ir.api.jvm.JIRMethod
 import org.opentaint.ir.api.jvm.cfg.JIRInst
 import org.opentaint.jvm.graph.JApplicationGraph
@@ -10,6 +11,7 @@ class JIRSafeApplicationGraph(
     override fun entryPoints(method: JIRMethod): Sequence<JIRInst> = try {
         graph.entryPoints(method)
     } catch (e: Throwable) {
+        logger.error(e) { "Method inst list failure $method" }
         // we couldn't find instructions list
         // TODO: maybe fix flowGraph()
         emptySequence()
@@ -18,8 +20,13 @@ class JIRSafeApplicationGraph(
     override fun exitPoints(method: JIRMethod): Sequence<JIRInst> = try {
         graph.exitPoints(method)
     } catch (e: Throwable) {
+        logger.error(e) { "Method inst list failure $method" }
         // we couldn't find instructions list
         // TODO: maybe fix flowGraph()
         emptySequence()
+    }
+
+    companion object {
+        private val logger = object : KLogging() {}.logger
     }
 }
