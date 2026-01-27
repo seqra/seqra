ceff6078cfc71b432623f499028c3aef36665ec2
Valentyn Sobol <8640896+saloed@users.noreply.github.com>
2026-01-27 13:23:47 +0300

    Initial column resolver

--- opentaint-dataflow-core/opentaint-dataflow/src/main/kotlin/org/opentaint/dataflow/sarif/SourceFileResolver.kt
+++ opentaint-dataflow-core/opentaint-dataflow/src/main/kotlin/org/opentaint/dataflow/sarif/SourceFileResolver.kt
@@ -1,9 +1,12 @@
 package org.opentaint.dataflow.sarif
 
 import org.opentaint.ir.api.common.cfg.CommonInst
+import java.nio.file.Path
 
 interface SourceFileResolver<in Statement : CommonInst> {
-    fun resolveByName(inst: Statement, pkg: String, name: String): String?
+    fun resolveByName(inst: Statement, pkg: String, name: String): Path?
 
-    fun resolveByInst(inst: Statement): String?
+    fun resolveByInst(inst: Statement): Path?
+
+    fun relativeToRoot(path: Path): String
 }
