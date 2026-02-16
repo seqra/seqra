3a5dea5b7d00649dc0b2bbdfb52061ef1e03c767
Daniil Logunov <43185213+danielelog@users.noreply.github.com>
2026-02-16 14:42:46 +0300

    Move config

---
+++ opentaint-config/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("java")
+}
+
+tasks.withType<ProcessResources> {
+    val configFile = layout.projectDirectory.file("config/config.yaml")
+
+    doLast {
+        check(configFile.asFile.exists()) { "Configuration file not found" }
+    }
+
+    from(configFile)
+}
