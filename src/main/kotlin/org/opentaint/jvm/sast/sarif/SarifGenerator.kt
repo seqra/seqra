38f0da45f8288c0f5e9b1f9bb6e13dd197fcab14
Valentyn Sobol <8640896+saloed@users.noreply.github.com>
2026-02-20 19:45:32 +0300

    Fix #285 (#287)

--- src/main/kotlin/org/opentaint/jvm/sast/sarif/SarifGenerator.kt
+++ src/main/kotlin/org/opentaint/jvm/sast/sarif/SarifGenerator.kt
@@ -132,7 +132,7 @@ class SarifGenerator(
             level = level,
             partialFingerprints = partialFingerPrints,
             locations = listOf(resolvedSinkLocation),
-            codeFlows = listOfNotNull(resolvedThreadFlows?.let { CodeFlow(threadFlows = it) })
+            codeFlows = resolvedThreadFlows.orEmpty().map { tf -> CodeFlow(threadFlows = listOf(tf)) }
         )
 
         for (annotator in annotators) {
