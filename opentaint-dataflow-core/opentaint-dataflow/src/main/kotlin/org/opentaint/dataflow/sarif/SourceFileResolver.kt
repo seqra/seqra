f4896013e27bfaa0bc2000953a6f84d660f33062
Valentyn Sobol <8640896+saloed@users.noreply.github.com>
2026-02-26 00:31:38 +0300

    Parametrize source file resolver

--- opentaint-dataflow-core/opentaint-dataflow/src/main/kotlin/org/opentaint/dataflow/sarif/SourceFileResolver.kt
+++ opentaint-dataflow-core/opentaint-dataflow/src/main/kotlin/org/opentaint/dataflow/sarif/SourceFileResolver.kt
@@ -3,10 +3,10 @@ package org.opentaint.dataflow.sarif
 import org.opentaint.ir.api.common.cfg.CommonInst
 import java.nio.file.Path
 
-interface SourceFileResolver<in Statement : CommonInst> {
-    fun resolveByName(inst: Statement, pkg: String, name: String): Path?
+interface SourceFileResolver<in Statement : CommonInst, Loc: Any> {
+    fun resolveByName(inst: Statement, pkg: String, name: String): Loc?
 
-    fun resolveByInst(inst: Statement): Path?
+    fun resolveByInst(inst: Statement): Loc?
 
     fun relativeToRoot(path: Path): String
 }
