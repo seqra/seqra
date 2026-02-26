4eab0cac67049284b42d7e1990262a5d872c666c
Valentyn Sobol <8640896+saloed@users.noreply.github.com>
2026-02-26 14:52:11 +0300

    Remove sarif utils

---
+++ opentaint-dataflow-core/opentaint-jvm-dataflow/src/main/kotlin/org/opentaint/dataflow/jvm/util/JirMethodExt.kt
@@ -0,0 +1,13 @@
+package org.opentaint.dataflow.jvm.util
+
+import org.opentaint.ir.api.jvm.JIRMethod
+import org.opentaint.ir.api.jvm.cfg.JIRCallExpr
+import org.opentaint.ir.api.jvm.cfg.JIRThis
+import org.opentaint.ir.api.jvm.ext.toType
+
+
+val JIRMethod.thisInstance: JIRThis
+    get() = JIRThis(enclosingClass.toType())
+
+val JIRCallExpr.callee: JIRMethod
+    get() = method.method
