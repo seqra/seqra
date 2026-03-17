d564abb311b3838e208acca132b0552b6db81dec
Aleksandr Misonizhnik <misonijnik@gmail.com>
2026-03-17 18:44:52 +0300

    refactor(analyzer): Move all JVM modules into opentaint-core/ subdirectory

--- opentaint-rules/test/buildSrc/settings.gradle.kts
+++ opentaint-rules/test/buildSrc/settings.gradle.kts
@@ -1,4 +1,4 @@
-includeBuild("../../../opentaint-common-build") {
+includeBuild("../../../opentaint-core/opentaint-common-build") {
     dependencySubstitution {
         substitute(module("org.opentaint:opentaint-common-build")).using(project(":"))
     }
--- opentaint-rules/test/settings.gradle.kts
+++ opentaint-rules/test/settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "seqra-builtin-rules-test"
 
-includeBuild("../../opentaint-sast-test-util") {
+includeBuild("../../opentaint-core/opentaint-sast-test-util") {
     dependencySubstitution {
         substitute(module("org.opentaint.sast-test-util:opentaint-sast-test-util")).using(project(":"))
     }
