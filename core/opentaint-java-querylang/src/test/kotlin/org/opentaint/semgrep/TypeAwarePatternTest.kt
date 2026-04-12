cddfe2d6530f9f895e70fd8fe2ccc0434bd121a5
Aleksandr Misonizhnik <misonijnik@gmail.com>
2026-04-13 00:30:46 +0300

    test: add E2E tests for type-aware pattern matching

---
+++ core/opentaint-java-querylang/src/test/kotlin/org/opentaint/semgrep/TypeAwarePatternTest.kt
@@ -0,0 +1,24 @@
+package org.opentaint.semgrep
+
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+import org.opentaint.semgrep.util.SampleBasedTest
+import kotlin.test.Test
+
+@TestInstance(PER_CLASS)
+class TypeAwarePatternTest : SampleBasedTest() {
+    @Test
+    fun `test generic type args in method parameter`() = runTest<example.RuleWithGenericTypeArgs>()
+
+    @Test
+    fun `test array return type matching`() = runTest<example.RuleWithArrayReturnType>()
+
+    @Test
+    fun `test concrete return type matching`() = runTest<example.RuleWithConcreteReturnType>()
+
+    @AfterAll
+    fun close() {
+        closeRunner()
+    }
+}
